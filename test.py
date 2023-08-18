1a6a50605857e216461f33a6bf4ce782196c44e5
SilhouetteCipher <141522054+SilhouetteCipher@users.noreply.github.com>
2023-08-18 21:50:46 +0100

    Update test.py

--- test.py
+++ test.py
@@ -116,7 +116,7 @@ for index, data in enumerate(mqtt_data):
     
     # Display topic labels
     short_topic = data["topic"].split("/")[-1]
-	label_width, label_height = draw.textsize(short_topic, font)
+    label_width, label_height = draw.textsize(short_topic, font)
     label_x = x + (bar_width - label_width) / 2
     label_y = inky_display.height - 22 + 5
     draw.text((label_x, label_y), short_topic, font=font, fill=inky_display.WHITE)
