712d65ee6a81178f2486bcbcccfbba5514a1cfcf
SilhouetteCipher <141522054+SilhouetteCipher@users.noreply.github.com>
2023-08-18 16:58:14 +0100

    t

--- testtext.py
+++ testtext.py
@@ -5,7 +5,7 @@ from PIL import Image
 display = InkyWHAT("yellow")  # Replace "yellow" with "red" if you have the red variant
 
 # Load the image using the Python Imaging Library (PIL)
-img = Image.open("/home/davvyk/Plants/test.jpeg")
+img = Image.open("/home/davvyk/Plants/test.jpg")
 
 # Set the image on the display and show it
 display.set_image(img)
