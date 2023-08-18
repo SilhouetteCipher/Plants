bc92ea2064c679a1ae756f06b850d7e17595c5a1
SilhouetteCipher <141522054+SilhouetteCipher@users.noreply.github.com>
2023-08-18 22:09:35 +0100

    removed labels box

--- test.py
+++ test.py
@@ -115,10 +115,18 @@ for index, data in enumerate(mqtt_data):
     draw.rectangle((x, y, x + bar_width, inky_display.height - 22), fill=inky_display.YELLOW)
     
     # Display topic labels
-    short_topic = data["topic"].split("/")[-1]
     label_width, label_height = draw.textsize(short_topic, font)
     label_x = x + (bar_width - label_width) / 2
-    label_y = inky_display.height - 22 + 2
+    label_y = inky_display.height - 22 + 5
+
+    padding = 5  # Adjust this value to increase/decrease the padding
+    background_rect = (
+        label_x - padding,
+        label_y - padding,
+        label_x + label_width + padding,
+        label_y + label_height + padding
+    )
+    draw.rectangle(background_rect, fill=inky_display.WHITE)
     draw.text((label_x, label_y), short_topic, font=font, fill=inky_display.BLACK)
 
 
