eb838d315483186993c7c6528bff9a85fe47f583
SilhouetteCipher <141522054+SilhouetteCipher@users.noreply.github.com>
2023-08-18 17:05:42 +0100

    Update testtext.py

--- testtext.py
+++ testtext.py
@@ -8,9 +8,33 @@ display = InkyWHAT("yellow")  # Replace "yellow" with "red" if you have the red 
 # Load the image using the Python Imaging Library (PIL)
 img = Image.open("/home/davvyk/Plants/test.jpg")
 
-img_gray = img.convert('L')
+# Convert image to RGB
+img_rgb = img.convert("RGB")
 
-# Set the image on the display and show it
+# Define the palette colors
+BLACK = (0, 0, 0)
+WHITE = (255, 255, 255)
+YELLOW = (255, 235, 59)  # An approximation; you might adjust this based on your design.
+
+def closest_color(rgb):
+    colors = [BLACK, WHITE, YELLOW]
+    closest_colors = sorted(colors, key=lambda color: sum((a-b) ** 2 for a, b in zip(color, rgb)))
+    return closest_colors[0]
+
+# Create a new image with the palette colors
+width, height = img_rgb.size
+new_img = Image.new("RGB", (width, height))
+
+for x in range(width):
+    for y in range(height):
+        pixel = img_rgb.getpixel((x, y))
+        new_img.putpixel((x, y), closest_color(pixel))
+
+# Save the processed image (optional)
+new_img.save("processed_image.png")
+
+# Use with Inky wHAT
+from inky.auto import auto
 display = auto()
-display.set_image(img)
+display.set_image(new_img)
 display.show()
