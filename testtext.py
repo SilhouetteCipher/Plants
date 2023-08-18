fdd9c83e8389e0f99002ada4e74142bd084fa5d7
SilhouetteCipher <141522054+SilhouetteCipher@users.noreply.github.com>
2023-08-18 16:52:33 +0100

    Update testtext.py

--- testtext.py
+++ testtext.py
@@ -1,3 +1,12 @@
-Test Text
-it worked
-it really did
+from inky import InkyWHAT
+from PIL import Image
+
+# Create an Inky wHAT display object
+display = InkyWHAT("yellow")  # Replace "yellow" with "red" if you have the red variant
+
+# Load the image using the Python Imaging Library (PIL)
+img = Image.open("/Plants/test.jpeg")
+
+# Set the image on the display and show it
+display.set_image(img)
+display.show()
