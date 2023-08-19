885c008221314f8179c1f910366a8ab6fd05e0b7
SilhouetteCipher <141522054+SilhouetteCipher@users.noreply.github.com>
2023-08-19 09:47:39 +0100

    Update test.py

--- test.py
+++ test.py
@@ -99,9 +99,8 @@ starting_x = 35.6
 font_path = "/home/davvyk/Plants/distinct.ttf"
 font_size = 20  # You can adjust this to your preference
 font = ImageFont.truetype(font_path, font_size)
-
 draw = ImageDraw.Draw(img)
-font = ImageFont.load_default()
+
 
 
 
